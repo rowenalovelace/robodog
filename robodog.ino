dac9f755575a65d79ced01ccf843c75438e5f13c
a a <a@a.com>
2017-12-29 22:04:29 +0100

    Walks ok

--- robodog.ino
+++ robodog.ino
@@ -31,34 +31,39 @@ void setup() {
    digitalWrite(10, LOW);
    digitalWrite(11, LOW);
 
-   delay(10000);
+   delay(5000);
 }
 
 void loop() {
   digitalWrite(LEG_MIDDLE_RIGHT, HIGH); // FEET UP
   digitalWrite(LEG_BACK_LEFT, HIGH);
   digitalWrite(LEG_FRONT_LEFT, HIGH);
-  delay(100);
-  digitalWrite(MOVE_FRONT_BACK_LEFT, HIGH);// FORWARD
+  delay(1000);
   digitalWrite(MOVE_MIDDLE_RIGHT, HIGH);
-  digitalWrite(MOVE_FRONT_BACK_RIGHT, LOW); //FORWARD
-  digitalWrite(MOVE_MIDDLE_LEFT, LOW);
-  delay(100);
-  digitalWrite(LEG_MIDDLE_RIGHT, LOW); // FEET DOWN
+  digitalWrite(MOVE_FRONT_BACK_LEFT, HIGH);// FORWARD
+  delay(1000);
+  digitalWrite(LEG_MIDDLE_RIGHT, LOW); // FEET UP
   digitalWrite(LEG_BACK_LEFT, LOW);
   digitalWrite(LEG_FRONT_LEFT, LOW);
-  delay(100);
+  delay(1000);
+
   digitalWrite(LEG_MIDDLE_LEFT, HIGH); // FEET UP
   digitalWrite(LEG_BACK_RIGHT, HIGH);
   digitalWrite(LEG_FRONT_RIGHT, HIGH);
-  delay(100);
-  digitalWrite(MOVE_FRONT_BACK_RIGHT, HIGH); //FORWARD
-  digitalWrite(MOVE_MIDDLE_LEFT, HIGH);
+  delay(1000);
+
+  digitalWrite(MOVE_MIDDLE_RIGHT, LOW); // FEET UP
   digitalWrite(MOVE_FRONT_BACK_LEFT, LOW);// FORWARD
-  digitalWrite(MOVE_MIDDLE_RIGHT, LOW);
-  delay(100);
-  digitalWrite(LEG_MIDDLE_LEFT, LOW); // FEET DOWn
+  delay(1000);
+
+  digitalWrite(MOVE_MIDDLE_LEFT, HIGH); // FEET UP
+  digitalWrite(MOVE_FRONT_BACK_RIGHT, HIGH);// FORWARD
+  delay(1000);
+
+
+  digitalWrite(LEG_MIDDLE_LEFT, LOW); // FEET UP
   digitalWrite(LEG_BACK_RIGHT, LOW);
   digitalWrite(LEG_FRONT_RIGHT, LOW);
-  delay(1000);                       // wait for a second
+  delay(1000);
+
 }
